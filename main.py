bfd6a06c64cf5280bea8ca61188739cdd07341cb
TNAV-2137 <tnav.2025@gmail.com>
2026-05-06 17:21:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide", page_title="Mapa PNA")
 
-st.title("Análisis de Dependencias - Prefectura Naval Argentina")
+st.title("Departamento Tecnico de la Navegacion - Inspectores")
 
 try:
     with open("Mapa_Inspectores_Actualizado.html", 'r', encoding='utf-8') as f:
         html_data = f.read()
     components.html(html_data, height=800, scrolling=True)
 except FileNotFoundError:
-    st.error("No se encontró el archivo del mapa")
+    st.error("No se encontró el archivo del mapa.")
